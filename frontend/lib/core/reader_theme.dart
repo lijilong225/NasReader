91e087db18791328ca0be1f89f2395042c1c4c45
longfen <lijilong225@gmail.com>
2026-08-23 09:30:47 +0800

    快速push

--- frontend/lib/core/reader_theme.dart
+++ frontend/lib/core/reader_theme.dart
@@ -1,33 +1,5 @@
 import 'package:flutter/material.dart';
 
-class ReaderThemes {
-  static const parchment = ReaderThemeData(
-    name: '羊皮纸',
-    bgColor: Color(0xFFF6EFE2),
-    textColor: Color(0xFF382E25),
-  );
-
-  static const eyeProtection = ReaderThemeData(
-    name: '护眼绿',
-    bgColor: Color(0xFFDDEBD6),
-    textColor: Color(0xFF233621),
-  );
-
-  static const dark = ReaderThemeData(
-    name: '暗夜',
-    bgColor: Color(0xFF1E1E1E),
-    textColor: Color(0xFF9E9E9E),
-  );
-
-  static const pureWhite = ReaderThemeData(
-    name: '白昼',
-    bgColor: Color(0xFFFFFFFF),
-    textColor: Color(0xFF1A1A1A),
-  );
-
-  static const all = [parchment, eyeProtection, dark, pureWhite];
-}
-
 class ReaderThemeData {
   final String name;
   final Color bgColor;
@@ -38,4 +10,29 @@ class ReaderThemeData {
     required this.bgColor,
     required this.textColor,
   });
+}
+
+class ReaderThemes {
+  static const parchment = ReaderThemeData(
+    name: '羊皮纸',
+    bgColor: Color(0xFFF6EFE2),
+    textColor: Color(0xFF382E25),
+  );
+  static const eyeCare = ReaderThemeData(
+    name: '护眼',
+    bgColor: Color(0xFFDDEBD6),
+    textColor: Color(0xFF233621),
+  );
+  static const dark = ReaderThemeData(
+    name: '暗黑',
+    bgColor: Color(0xFF1E1E1E),
+    textColor: Color(0xFF9E9E9E),
+  );
+  static const white = ReaderThemeData(
+    name: '纯白',
+    bgColor: Color(0xFFFFFFFF),
+    textColor: Color(0xFF1A1A1A),
+  );
+
+  static const List<ReaderThemeData> all = [parchment, eyeCare, dark, white];
 }

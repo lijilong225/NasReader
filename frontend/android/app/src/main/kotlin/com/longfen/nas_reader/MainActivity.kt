eab023aa7138304d5e1b3bba523109263d10094c
longfen <lijilong225@gmail.com>
2026-08-23 10:24:02 +0800

    快速push

---
+++ frontend/android/app/src/main/kotlin/com/longfen/nas_reader/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.example.nas_reader // 确保与 AndroidManifest.xml 和 build.gradle 的包名一致
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+    // Flutter 3.x+ V2 Embedding 会自动注册所有插件
+    // 无需重写任何 configureFlutterEngine 或 registerWith 方法
+}

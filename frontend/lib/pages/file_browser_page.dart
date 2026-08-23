5c5e8338451cdd3441b454f9c1a2d06552615ae9
longfen <lijilong225@gmail.com>
2026-08-23 15:43:30 +0800

    快速push

--- frontend/lib/pages/file_browser_page.dart
+++ frontend/lib/pages/file_browser_page.dart
@@ -73,7 +73,7 @@ class _FileBrowserPageState extends State<FileBrowserPage> {
   Future<void> _fetchDirectory(String path) async {
     setState(() => _isLoading = true);
     try {
-      final res = await widget.dio.get('/api/files/list', queryParameters: {'path': path});
+      final res = await widget.dio.get('/api/v1/files/list', queryParameters: {'path': path});
       if (res.statusCode == 200 && res.data['code'] == 0) {
         setState(() {
           _currentPath = path;
@@ -222,7 +222,7 @@ class _FileBrowserPageState extends State<FileBrowserPage> {
       if (!file.existsSync()) {
         _showLoadingDialog('正在从 NAS 下载...');
         await widget.dio.download(
-          '/api/files/download',
+          '/api/v1/files/download',
           savePath,
           queryParameters: {'path': remotePath},
         );
